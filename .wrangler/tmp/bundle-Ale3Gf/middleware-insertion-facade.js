a305b62dd1302474a2b78bb650193f19854e4790
Dom Cogan <80106370+Dom-Cogan@users.noreply.github.com>
2024-04-26 14:16:49 -0400

    wrangleer update

---
+++ .wrangler/tmp/bundle-Ale3Gf/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/domcogan/Development/Apps/Dom-Cogan/.wrangler/tmp/pages-bgVyt7/iff6crltxa.js";
+				import * as __MIDDLEWARE_0__ from "/Users/domcogan/Development/Apps/Dom-Cogan/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/domcogan/Development/Apps/Dom-Cogan/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+				
+				worker.middleware = [
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default,
+					...(worker.middleware ?? []),
+				].filter(Boolean);
+				
+				export * from "/Users/domcogan/Development/Apps/Dom-Cogan/.wrangler/tmp/pages-bgVyt7/iff6crltxa.js";
+				export default worker;
